b586aea2de4c6047ed379c220ca80c04ac4a3c25
luka270903 <117845518+luka270903@users.noreply.github.com>
2024-01-25 14:31:42 +0100

    func voor meerdere timers zonder elke keer opnieuw input.py te moeten runnen

--- TI Timer/input.py
+++ TI Timer/input.py
@@ -11,13 +11,27 @@ def read_serial(port):
 
 serial_ports = list_ports.comports()
 
-pico_port = serial_ports[0].device
+# als het niet bekend is op welke poort de pico is aangesloten
+print("[INFO] Serial ports found:")
+for i, port in enumerate(serial_ports):
+    print(str(i) + ". " + str(port.device))
+
+pico_port_index = int(input("Which port is the Raspberry Pi Pico connected to? "))
+pico_port = serial_ports[pico_port_index].device
+
+# als het wel bekend is welke poort het is kan je dat hardcoden
+# pico_port = serial_ports[0].device
 
 # Open a connection to the Pico
 with serial.Serial(port=pico_port, baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=1) as serial_port:
-    time_in_seconds = input("How long do you want the timer to be in seconds?:")
+    while True:
 
-    time_in_seconds_pico_acceptable = time_in_seconds + "\r"
-    serial_port.write(time_in_seconds_pico_acceptable.encode())
-    pico_output = read_serial(serial_port)
-serial_port.close()
+        timer_time = int(input("How long do you want the timer to be in seconds? Type '0' to exit: "))
+
+        if timer_time == 0:
+            serial_port.close()
+            break
+        else:
+            timer_time_str = str(timer_time) + "\r"
+            serial_port.write(timer_time_str.encode())
+            pico_output = read_serial(serial_port)
